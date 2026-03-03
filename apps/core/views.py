6ef652ccde574f4d413eb40039fdb4438cf83b61
Mazen Tayseer <maziiin360@gmail.com>
2026-03-03 14:47:21 +0200

    Refactor CoreView to use ViewSet instead of GenericViewSet.

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ViewSet
 
 
-class CoreView(GenericViewSet):
+class CoreView(ViewSet):
     def list(self, request):
         return Response({"message": "Hello, World!"})
